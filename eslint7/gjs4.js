8afc964e91c97f487aa7b67ff86467843d90f82e
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-04-27 07:45:47 -0700

    updated some GJS4 rules

--- eslint7/gjs4.js
+++ eslint7/gjs4.js
@@ -1,16 +1,26 @@
 'use strict';
 
 // https://gjs.guide/guides/gjs/intro.html
+// https://gjs.guide/guides/gjs/features-across-versions.html
 
 const rules = {
-    indent: [1, 2],
+    camelcase: 1,
+    'func-call-spacing': [1, 'never'],
+    indent: [1, 4],
+    'no-console': 'error', // https://gjs.guide/guides/gjs/intro.html#logging
+    'no-tabs': 1,
+    'no-trailing-spaces': 'warn',
+    'prefer-template': 'warn',
+    'object-curly-spacing': [1, 'always'], // https://gjs.guide/guides/gjs/style-guide.html#use-spaces-inside-curly-braces
+    'key-spacing': [1, {beforeColon: false, afterColon: true}], // https://gjs.guide/guides/gjs/style-guide.html#enforce-spacing-between-keys-and-values-in-object-literal-properties
     quotes: [1, 'single'],
     semi: [2, 'always']
 };
 
 const globals = {
-    imports: 'readonly',
-    exports: 'writable'
+    exports: 'writable',
+    imports: 'readonly',  // https://gjs.guide/guides/gjs/intro.html#imports-and-modules
+    log: 'readonly'       // https://gjs.guide/guides/gjs/intro.html#logging
 };
 
 module.exports = {
